a8312b488c2504ec70859cb3ae1f33746c5615a6
RyanD <rkdupe@gmail.com>
2022-09-27 05:47:15 -0400

    Add project files.

---
+++ StringCalculatorKata092722/StringCalculator.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace StringCalculatorKata092722;
+
+public class StringCalculator
+{
+    public int Add(string numbers)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ StringCalculatorKata092722/StringCalculatorTests.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace StringCalculatorKata092722;
+
+[TestClass]
+public class StringCalculatorTests
+{
+    [TestMethod]
+    public void Add_EmptyString_ShouldReturn_Zero()
+    {
+        StringCalculator sc = new StringCalculator();
+
+        int expected = 0;
+
+        int actual = sc.Add("");
+
+        Assert.AreEqual(expected, actual);
+    }
+}
