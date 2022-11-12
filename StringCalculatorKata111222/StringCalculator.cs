d50c68b511a9189ff9b197e76ce59b9bfea434df
RyanD <rkdupe@gmail.com>
2022-11-12 08:29:53 -0500

    Completed Step 1 - Simple Calculator

---
+++ StringCalculatorKata111222/StringCalculator.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace StringCalculatorKata111222;
+
+public static class StringCalculator
+{
+    private static string _input = string.Empty;
+
+    public static int Add(string input)
+    {
+        _input = input;
+
+        if(string.IsNullOrEmpty(_input))
+            return 0;
+
+        List<string> inputList = _input.Split(",").ToList();
+
+        return inputList.Count == 2 ?
+            int.Parse(inputList[0]) + int.Parse(inputList[1]) :
+            int.Parse(inputList[0]);
+    }
+}
---
+++ StringCalculatorKata111222/StringCalculatorTests.cs
@@ -0,0 +1,37 @@
+﻿
+
+namespace StringCalculatorKata111222;
+
+[TestClass]
+public class StringCalculatorTests
+{
+    [TestMethod]
+    public void Add_EmptyString_ShouldReturn_Zero()
+    {
+        int expected = 0;
+
+        int actual = StringCalculator.Add(string.Empty);
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void Add_StringNumber_ShouldReturn_Number()
+    {
+        int expected = 5;
+
+        int actual = StringCalculator.Add("5");
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void Add_TwoNums_ShouldReturn_Sum()
+    {
+        int expected = 7;
+
+        int actual = StringCalculator.Add("5,2");
+
+        Assert.AreEqual(expected, actual);
+    }
+}
